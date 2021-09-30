e4b670c7352a8788fd6d8e329393a48af6c87a13
wanglonghong <wanglonghong4467@gmail.com>
2021-09-30 23:16:06 +0800

    Update AlturaNFTAuction.sol

--- contracts/AlturaNFTAuction.sol
+++ contracts/AlturaNFTAuction.sol
@@ -66,6 +66,7 @@ contract AlturaNFTAuction is UUPSUpgradeable, ERC1155HolderUpgradeable, OwnableU
         address payable from;
 		address currency;
         uint256 amount;
+		bool    active;
     }
 
 	// auction id => Item mapping
@@ -77,8 +78,8 @@ contract AlturaNFTAuction is UUPSUpgradeable, ERC1155HolderUpgradeable, OwnableU
     
     // Mapping from owner to a list of owned auctions
     mapping (address => uint256[]) public ownedAuctions;
+	mapping(uint256 => mapping(address => uint256)) private auctionUserBids;
 
-    
     uint256 public totalSold;  /* Total NFT token amount sold */
 	uint256 public totalEarning; /* Total Plutus Token */
 	uint256 public totalSwapped; /* Total swap count */
@@ -91,7 +92,8 @@ contract AlturaNFTAuction is UUPSUpgradeable, ERC1155HolderUpgradeable, OwnableU
 	event AuctionCancelled(uint256 id, Auction auction);
 	event AuctionFinalized(uint256, Auction auction);
 
-	event BidSuccess(address from, uint auctionId, uint256 price, address currency, uint bidIndex);
+	event NewBid(address from, uint auctionId, uint256 price, address currency, uint bidIndex);
+	event BidCancelled(uint auctionId, uint bidIndex);
 
 
 	function initialize(address _fee) public initializer {
@@ -170,6 +172,183 @@ contract AlturaNFTAuction is UUPSUpgradeable, ERC1155HolderUpgradeable, OwnableU
         emit AuctionCancelled(_auctionId, myAuction);
     }
 
+
+	/**
+     * @dev Finalized an ended auction
+     * @dev The auction should be ended, and there should be at least one bid
+     * @dev On success Deed is transfered to bidder and auction owner gets the amount
+     * @param _auctionId uint ID of the created auction
+     */
+    function finalizeAuction(uint _auctionId, uint _bidIdx) public {
+        Auction storage myAuction = auctions[_auctionId];
+        uint bidsLength = auctionBids[_auctionId].length;
+
+		require(myAuction.active, "auction is not active");
+
+        // 1. if auction not ended just revert
+        require(msg.sender == owner() || myAuction.isUnlimitied || block.timestamp >= myAuction.endTime, "auction is not ended");
+        require(msg.sender == myAuction.owner || msg.sender == owner(), "only auction owner can finalize");
+        
+        // if there are no bids cancel
+        if(bidsLength == 0) {
+            cancelAuction(_auctionId);
+        } else {
+			if(!myAuction.isUnlimitied) {
+				Bid memory lastBid = auctionBids[_auctionId][bidsLength - 1];
+
+				if(lastBid.amount > 0) {
+					_distributeBid(myAuction.collectionId, myAuction.tokenId, lastBid.currency, address(this), _msgSender(), lastBid.amount);
+					IAlturaNFT(myAuction.collectionId).safeTransferFrom(address(this), lastBid.from, myAuction.tokenId, 1, "finalize auction");
+				}
+			} else {
+				require(_bidIdx < auctionBids[_auctionId].length, "invalid bid index");
+				Bid storage choosenBid = auctionBids[_auctionId][_bidIdx];
+                require(choosenBid.active && choosenBid.amount > 0, "selected bid is not active");
+
+                _distributeBid(myAuction.collectionId, myAuction.tokenId, choosenBid.currency, address(this), _msgSender(), choosenBid.amount);
+				IAlturaNFT(myAuction.collectionId).safeTransferFrom(address(this), choosenBid.from, myAuction.tokenId, 1, "finalize auction");
+
+				choosenBid.active = false;
+			}
+			
+
+            myAuction.active = false;
+            myAuction.finalized = true;
+            emit AuctionFinalized(_auctionId, myAuction);
+        }
+    }
+
+	function _distributeBid(address collection, uint256 tokenId, address paymentToken, address from, address to, uint256 amount) internal {
+        address _creator = IAlturaNFT(collection).creatorOf(tokenId);
+		uint256 royalties = IAlturaNFT(collection).royaltyOf(tokenId);
+		// % commission cut
+		uint256 swapFee = swapFees[paymentToken];
+		if(swapFee == 0x0) {
+			swapFee = DEFAULT_FEE_PERCENT;
+		}
+
+		uint256 _commissionValue = amount.mul(swapFee).div(PERCENTS_DIVIDER);
+		uint256 _royalties = (amount.sub(_commissionValue)).mul(royalties).div(PERCENTS_DIVIDER);
+		uint256 _sellerValue = amount.sub(_commissionValue).sub(_royalties);
+
+        if(paymentToken == address(0x0)) {
+			require(_safeTransferBNB(to, _sellerValue), "transfer to seller failed");
+			if(_commissionValue > 0) _safeTransferBNB(feeAddress, _commissionValue);
+			if(_royalties > 0) _safeTransferBNB(_creator, _royalties);
+		}
+		else {
+			require(IERC20(paymentToken).transfer(to, _sellerValue), "transfer to seller failed");
+			if(_commissionValue > 0) require(IERC20(paymentToken).transfer(feeAddress, _commissionValue));
+			if(_royalties > 0) require(IERC20(paymentToken).transfer(_creator, _royalties));
+		}
+    }
+
+	/**
+     *  Bidder sends bid on an auction
+     *  Auction should be active and not ended
+     * @param _id uint256 ID of the created auction
+     * @param amount uint256 amount of bid
+     */
+    function placeBid(uint256 _id, address currency, uint256 amount) public payable {
+        // owner can't bid on their auctions
+        Auction storage myAuction = auctions[_id];
+        require(myAuction.active, "Auction is not acitve");
+        require(myAuction.owner != _msgSender(), "owner can not bid");
+        require(myAuction.isUnlimitied || myAuction.currency == currency, "Only OpenBid supports custom Token Payment");
+
+        // Check already placed bid
+        uint256 bidIndex = auctionUserBids[_id][_msgSender()];
+		Bid storage oldBid = auctionBids[_id][bidIndex];
+        require(!oldBid.active || oldBid.currency == currency, "not matched with old bid");
+
+
+        uint256 tempAmount = myAuction.startPrice;
+        // if auction is timelimited auction
+        if(!myAuction.isUnlimitied) {
+            require(block.timestamp < myAuction.endTime, "auction is over");
+            require(block.timestamp >= myAuction.startTime, "auction is not started");
+
+            // check if bid amount is bigger than lastBid
+            if(getBidsLength(_id) > 0) {
+                Bid memory lastBid = auctionBids[_id][getBidsLength(_id).sub(1)];
+                tempAmount = lastBid.amount;
+
+				if(lastBid.from != _msgSender()) {
+					require(amount > tempAmount, "TOO_SMALL_AMOUNT");
+
+					// refund last bid 
+					if(myAuction.currency == address(0x0)) {
+						require(_safeTransferBNB(lastBid.from, lastBid.amount), "refund to last bidder failed");
+					}
+					else {
+						require(IERC20(myAuction.currency).transfer(lastBid.from, lastBid.amount), "refund to last bidder failed");
+					}
+				}
+            }
+			else {
+				require(amount > tempAmount, "amount is smaller than start price");
+			}
+			
+        }
+
+        
+        // transfer Payment Token to Auction contract from bidder
+        if(myAuction.currency == address(0x0)) {
+            require(amount == msg.value, "INVALID_BNB_VALUE");
+        } else {
+			IERC20(myAuction.currency).transferFrom(_msgSender(),  address(this), amount);
+        }
+        
+        // check already placed bid
+		// timelimted auction => lasted bid
+        if(oldBid.active) {
+            oldBid.amount = oldBid.amount.add(amount);
+        }
+        else {
+            // add bid to Store
+            Bid memory newBid;
+            newBid.from     = payable(_msgSender());
+            newBid.currency   = currency;
+            newBid.amount     = amount;
+            newBid.active     = true;
+            
+			auctionBids[_id].push(newBid);
+			
+			bidIndex = getBidsLength(_id).sub(1);
+			auctionUserBids[_id][_msgSender()] = bidIndex;
+        }
+
+        emit NewBid(_msgSender(), _id, amount, currency, bidIndex);
+    }
+
+
+	/**
+     *  Bidder withdraw Bid
+     *  Bid should be active
+     *  TimedAuction: Bidder can withdraw only after Auction ended and decided winning bid
+     *  OpenBid:  Bidder can withdraw anytime
+     * @param _id uint256 ID of the created auction
+     */
+    function cancelBid(uint256 _id) public nonReentrant {
+        Auction storage myAuction = auctions[_id];
+		require(myAuction.isUnlimitied, "only cancel free bid");
+
+        uint256 bidIndex = auctionUserBids[_id][_msgSender()];
+		Bid storage bid = auctionBids[_id][bidIndex];
+
+        require(bid.from == _msgSender(), "not owner");
+        require(bid.active, "bid is not active");
+        
+         // If this auction is open bid, bidder can withdraw anytime
+        IERC20(bid.currency).transfer(_msgSender(), bid.amount);
+
+        // close Bid item
+       	bid.active = false;
+
+        emit BidCancelled(_id, bidIndex);
+    }
+
+
 	function _safeTransferBNB(address to, uint256 value) internal returns(bool) {
 		(bool success, ) = to.call{value: value}(new bytes(0));
 		if(!success) {
@@ -192,7 +371,7 @@ contract AlturaNFTAuction is UUPSUpgradeable, ERC1155HolderUpgradeable, OwnableU
      * @dev Gets the bid counts of a given auction
      * @param _auctionId uint ID of the auction
      */
-    function getBidsAmount(uint _auctionId) public view returns(uint) {
+    function getBidsLength(uint _auctionId) public view returns(uint) {
         return auctionBids[_auctionId].length;
     } 
     
@@ -210,7 +389,7 @@ contract AlturaNFTAuction is UUPSUpgradeable, ERC1155HolderUpgradeable, OwnableU
      * @param _auctionId uint of the auction owner
      * @return amount uint256, address of last bidder
      */
-    function getCurrentBids(uint _auctionId) public view returns(uint256, address) {
+    function getLastBid(uint _auctionId) public view returns(uint256, address) {
         uint bidsLength = auctionBids[_auctionId].length;
         // if there are bids refund the last bid
         if (bidsLength >= 0) {
@@ -225,7 +404,7 @@ contract AlturaNFTAuction is UUPSUpgradeable, ERC1155HolderUpgradeable, OwnableU
      * @param _owner address of the owner
      * @return uint total number of auctions
      */
-    function getAuctionsAmount(address _owner) public view returns(uint) {
+    function getOwnedAuctionsLength(address _owner) public view returns(uint) {
         return ownedAuctions[_owner].length;
     }
 
